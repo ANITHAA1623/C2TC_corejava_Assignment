da32beb6cdd6cfc6317f73880ece1bb72a2c82cb
STUDENT <STUDENT@DESKTOP-E447JQ5>
2025-08-19 11:51:38 +0530

    assignment2 is completed

---
+++ C2Tc_corejavaAssignment/src/com/anitha/assignament2/student.java
@@ -0,0 +1,15 @@
+package com.anitha.assignament2;
+
+public class student {
+
+
+    // Default Constructor
+    student() {
+        System.out.println("Student object is created");
+    }
+
+    public static void main(String[] args) {
+        // When object is created, constructor runs automatically
+        student s1 = new student();
+    }
+}
